53b57f125ada223ef949eb8d134bc4c66a19457c
genesiski <120064239+genesiski@users.noreply.github.com>
2023-03-31 22:11:46 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 
+const movieRouter = require('./movies/movies.router')
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Server OK'})
+})
+
+app.use('/', movieRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`server started at port ${PORT}`)
+})
 
 module.exports = app
